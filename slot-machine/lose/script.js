1029e1f5d0c94505dbd6a26152380e362dd4c064
YJL37 <yjlee3731@gmail.com>
2024-03-27 11:15:59 +0900

    Fire effects to lose

--- slot-machine/lose/script.js
+++ slot-machine/lose/script.js
@@ -187,3 +187,13 @@ $('#btn-example1').click(function() {
      
   });
 });
+
+function fire() {
+    $.each($(".particletext.fire"), function(){
+       var firecount = ($(this).width()/50)*20;
+       for(var i = 0; i <= firecount; i++) {
+          var size = $.rnd(8,12);
+          $(this).append('<span class="particle" style="top:' + $.rnd(40,70) + '%; left:' + $.rnd(-10,100) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,20)/10) + 's;"></span>');
+       }
+    });
+ }
